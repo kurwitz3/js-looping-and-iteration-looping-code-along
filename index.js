ab3988935db6ef097a49235c1784f05da8b249cd
kurwitz3 <kurwitz3@aol.com>
2021-01-21 21:05:12 -0500

    commit tree 7c2b0b432e9502a38d35a54b70682b6aaefea770

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(array,event){
+    let thanksArray = []
+  for(let i = 0;i < array.length; i++){
+      
+      thanksArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+      
+  }
+  return thanksArray
+}
+
+function countDown(number){
+    while(number >= 0){
+    console.log(number)
+    number--
+}
+   
+}
